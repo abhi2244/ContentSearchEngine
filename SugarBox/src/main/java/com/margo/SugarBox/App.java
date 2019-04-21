6fc9af8019a4a1469c355a4e5f310ce58b87faea
Pulkit2244 <abhi.iitgn@gmail.com>
2019-04-21 16:35:12 +0530

    Commited all initial files

---
+++ SugarBox/src/main/java/com/margo/SugarBox/App.java
@@ -0,0 +1,13 @@
+package com.margo.SugarBox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
